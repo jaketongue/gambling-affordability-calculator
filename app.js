9b827426ecdd03c82eeddb0f5a7c0ad572d89ab6
Jake Tongue <jake.tongue@skybettingandgaming.com>
2020-02-28 12:07:56 +0000

    Converting into a node project

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('css'));
+app.use(express.static('images'));
+app.use(express.static('scripts'));
+
+app.get('/', function(req, res) {
+  res.sendFile('/Users/jto01/Desktop/codebases/gambling-affordability-calculator-copy/views/' + 'landingPage.html');
+});
+
+app.get('/calculatorPage.html', function(req,res) {
+  res.sendFile('/Users/jto01/Desktop/codebases/gambling-affordability-calculator-copy/views/' + 'calculatorPage.html');
+});
+
+app.get('/summaryPage.html', function(req,res) {
+  res.sendFile('/Users/jto01/Desktop/codebases/gambling-affordability-calculator-copy/views/' + 'summaryPage.html');
+});
+
+app.listen(8000, function () {
+});
