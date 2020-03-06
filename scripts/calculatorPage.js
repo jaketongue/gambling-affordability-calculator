ee0e40492d8204c562cb09c42bdbeff507539f16
Jake Tongue <jake.tongue@skybettingandgaming.com>
2020-03-06 15:15:15 +0000

    Jest unit tests

--- scripts/calculatorPage.js
+++ scripts/calculatorPage.js
@@ -1,83 +1,106 @@
-function calculateValidatedAffordability() {
-
-    var a = document.getElementById('reviewTotalIncome').value;
-    var b = document.getElementById('reviewTotalHousehold').value;
-    var c = document.getElementById('reviewTotalAdditional').value;
-    var d = document.getElementById('reviewTotalGambling').value;
-    if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
+function calculateValidatedAffordability(testStore = "") {
+    var totalIncome = testStore.totalIncome || document.getElementById('reviewTotalIncome').value;
+    var totalHousehold = testStore.totalHousehold || document.getElementById('reviewTotalHousehold').value;
+    var totalAdditional = testStore.totalAdditional || document.getElementById('reviewTotalAdditional').value;
+    var totalGambling = testStore.totalGambling || document.getElementById('reviewTotalGambling').value;
+    
+    if (totalIncome == null || totalIncome == "", totalHousehold == null || totalHousehold == "", totalAdditional == null || totalAdditional == "", totalGambling == null || totalGambling == "") {
       alert("Please fill in the required fields.");
       return false;
     
     } else {
-        var reviewTotalIncome = parseInt(document.getElementById('reviewTotalIncome').value);
-        var reviewTotalHousehold = parseInt(document.getElementById('reviewTotalHousehold').value);
-        var reviewTotalAdditional = parseInt(document.getElementById('reviewTotalAdditional').value);
-        var reviewTotalGambling = parseInt(document.getElementById('reviewTotalGambling').value);
-        var nanChecker = typeof(NaN);
-        if (true){
-            var calculateAffordability = (reviewTotalIncome) - (reviewTotalHousehold + reviewTotalAdditional + reviewTotalGambling);
-            var calculateSpendingDifference = (calculateAffordability - reviewTotalGambling);
-    
-            localStorage.setItem("disposableIncome", calculateAffordability);
-            localStorage.setItem("spendingDifference", calculateSpendingDifference);
-            localStorage.setItem("reviewTotalGambling", reviewTotalGambling);
-    
-            window.location.href = "./summaryPage.html";
+        var reviewTotalIncome = parseInt(totalIncome);
+        var reviewTotalHousehold = parseInt(totalHousehold);
+        var reviewTotalAdditional = parseInt(totalAdditional);
+        var reviewTotalGambling = parseInt(totalGambling);
 
-        } else {
-            alert('Oops. Something went wrong!');
+        var calculateAffordability = (reviewTotalIncome) - (reviewTotalHousehold + reviewTotalAdditional + reviewTotalGambling);
+        var calculateSpendingDifference = (calculateAffordability - reviewTotalGambling);
+
+        if (testStore !== "") {
+            return {
+                calculateAffordability,
+                calculateSpendingDifference
+            };
         }
+
+        localStorage.setItem("disposableIncome", calculateAffordability);
+        localStorage.setItem("spendingDifference", calculateSpendingDifference);
+        localStorage.setItem("reviewTotalGambling", reviewTotalGambling);
+
+        window.location.href = "./summaryPage.html";
     }
 }
 
-
-function incomeFunction() {
-
-    var salary = parseInt(document.getElementById('salary').value);
-    var bonus = parseInt(document.getElementById('bonus').value);
-    var other = parseInt(document.getElementById('other').value);
-    var pension = parseInt(document.getElementById('pension').value);
-    var savings = parseInt(document.getElementById('savings').value);
+function incomeFunction(testStore = "") {
+    var salary = testStore.salary || parseInt(document.getElementById('salary').value);
+    var bonus = testStore.bonus || parseInt(document.getElementById('bonus').value);
+    var other = testStore.other || parseInt(document.getElementById('other').value);
+    var pension = testStore.pension || parseInt(document.getElementById('pension').value);
+    var savings = testStore.savings || parseInt(document.getElementById('savings').value);
     var totalincome = (salary + bonus + other) - (pension + savings);
+
+    if (testStore !== "") {
+        return totalincome;
+    }
 
     document.getElementById("totalincome").value = totalincome;
     document.getElementById("reviewTotalIncome").value = totalincome;
 }
 
-function householdFunction() {
+function householdFunction(testStore = "") {
     
-    var mortgage = parseInt(document.getElementById('mortgage').value);
-    var rent = parseInt(document.getElementById('rent').value);
-    var council = parseInt(document.getElementById('council').value);
-    var utilities = parseInt(document.getElementById('utilities').value);
-    var tech = parseInt(document.getElementById('tech').value);
-    var food = parseInt(document.getElementById('food').value);
-    var insurance = parseInt(document.getElementById('insurance').value);
+    var mortgage = testStore.mortgage || parseInt(document.getElementById('mortgage').value);
+    var rent = testStore.rent || parseInt(document.getElementById('rent').value);
+    var council = testStore.council || parseInt(document.getElementById('council').value);
+    var utilities = testStore.utilities || parseInt(document.getElementById('utilities').value);
+    var tech = testStore.tech || parseInt(document.getElementById('tech').value);
+    var food = testStore.food || parseInt(document.getElementById('food').value);
+    var insurance = testStore.insurance || parseInt(document.getElementById('insurance').value);
     var totalhousehold = (mortgage + rent + council + utilities + tech + food + insurance);
+
+    if (testStore !== "") {
+        return totalhousehold;
+    }
     
     document.getElementById("totalhousehold").value = totalhousehold;
     document.getElementById("reviewTotalHousehold").value = totalhousehold;
 }
 
-function additionalFunction() {
+function additionalFunction(testStore = "") {
 
-    var travel = parseInt(document.getElementById('travel').value);
-    var loan = parseInt(document.getElementById('loan').value);
-    var leisure = parseInt(document.getElementById('leisure').value);
-    var othershop = parseInt(document.getElementById('othershop').value);
+    var travel = testStore.travel || parseInt(document.getElementById('travel').value);
+    var loan = testStore.loan || parseInt(document.getElementById('loan').value);
+    var leisure = testStore.leisure || parseInt(document.getElementById('leisure').value);
+    var othershop = testStore.othershop || parseInt(document.getElementById('othershop').value);
     var totaladditional = (travel + loan + leisure + othershop);
+
+    if (testStore !== "") {
+        return totaladditional;
+    }
 
     document.getElementById("totaladditional").value = totaladditional;
     document.getElementById("reviewTotalAdditional").value = totaladditional;
 }
 
-function gamblingFunction() {
-    
-    var sports = parseInt(document.getElementById('sports').value);
-    var gaming = parseInt(document.getElementById('gaming').value);
-    var othergam = parseInt(document.getElementById('othergam').value);
+function gamblingFunction(testStore = "") {
+    var sports = testStore.sports || parseInt(document.getElementById('sports').value);
+    var gaming = testStore.gaming || parseInt(document.getElementById('gaming').value);
+    var othergam = testStore.othergam || parseInt(document.getElementById('othergam').value);
     var totalgambling = (sports + gaming + othergam);
     
+    if (testStore !== "") {
+        return totalgambling;
+    }
+
     document.getElementById("totalgambling").value = totalgambling;
     document.getElementById("reviewTotalGambling").value = totalgambling;
 }
+
+module.exports = {
+    calculateValidatedAffordability,
+    incomeFunction,
+    householdFunction,
+    additionalFunction,
+    gamblingFunction
+};
---
+++ scripts/calculatorPage.test.js
@@ -0,0 +1,114 @@
+const methodsToTest = require('./calculatorPage');
+const calculateValidatedAffordability = methodsToTest.calculateValidatedAffordability;
+const incomeFunction = methodsToTest.incomeFunction;
+const householdFunction = methodsToTest.householdFunction;
+const additionalFunction = methodsToTest.additionalFunction;
+const gamblingFunction = methodsToTest.gamblingFunction;
+
+describe("Testing calculatorPage functionality", () => {
+
+    test("Testing affordability and spending difference values are calculating the correct values", () => {
+        const totalIncomeTest = 10;
+        const totalHouseholdTest = 2;
+        const totalAdditionalTest = 3;
+        const totalGamblingTest = 4;
+        const expectedTotalAffordability = (totalIncomeTest) - (totalHouseholdTest+totalAdditionalTest+totalGamblingTest);
+        const expectedTotalDifference = (expectedTotalAffordability) - (totalGamblingTest);
+
+        const testStore = {
+            totalIncome: totalIncomeTest,
+            totalHousehold: totalHouseholdTest,
+            totalAdditional: totalAdditionalTest,
+            totalGambling: totalGamblingTest,
+        };
+
+        const result = calculateValidatedAffordability(testStore);
+
+        expect(result.calculateAffordability).toEqual(expectedTotalAffordability);
+        expect(result.calculateSpendingDifference).toEqual(expectedTotalDifference);
+    })
+
+    test("Testing total income value is the addition of the correct values", () => {
+        const salaryTest = 1;
+        const bonusTest = 2;
+        const otherTest = 3;
+        const pensionTest = 4;
+        const savingsTest = 5;
+        const expectedTotal = (salaryTest+bonusTest+otherTest)-(pensionTest+savingsTest);
+
+        const testStore = {
+            salary: salaryTest,
+            bonus: bonusTest,
+            other: otherTest,
+            pension: pensionTest,
+            savings: savingsTest,
+        };
+
+        const result = incomeFunction(testStore);
+
+        expect(result).toEqual(expectedTotal);
+    })
+
+    test("Testing total household spending value is the addition of the correct values", () => {
+        const mortgageTest = 1;
+        const rentTest = 2;
+        const councilTest = 3;
+        const utilitiesTest = 4;
+        const techTest = 5;
+        const foodTest = 6;
+        const insuranceTest = 7;
+        const expectedTotal = mortgageTest+rentTest+councilTest+utilitiesTest+techTest+foodTest+insuranceTest;
+
+        const testStore = {
+            mortgage: mortgageTest,
+            rent: rentTest,
+            council: councilTest,
+            utilities: utilitiesTest,
+            tech: techTest,
+            food: foodTest,
+            insurance: insuranceTest,
+        };
+
+        const result = householdFunction(testStore);
+
+        expect(result).toEqual(expectedTotal);
+    })
+
+    test("Testing total additional spending value is the addition of the correct values", () => {
+        const travelTest = 1;
+        const loanTest = 2;
+        const leisureTest = 3;
+        const othershopTest = 4;
+        const expectedTotal = travelTest+loanTest+leisureTest+othershopTest;
+
+        const testStore = {
+            travel: travelTest,
+            loan: loanTest,
+            leisure: leisureTest,
+            othershop: othershopTest
+        };
+
+        const result = additionalFunction(testStore);
+
+        expect(result).toEqual(expectedTotal);
+    })
+
+    test("Testing total gambling value is the addition of the correct values", () => {
+        const sportsTest = 1;
+        const gamingTest = 2;
+        const othergamTest = 3;
+        const expectedTotal = sportsTest+gamingTest+othergamTest;
+
+        const testStore = {
+            sports: sportsTest,
+            gaming: gamingTest,
+            othergam: othergamTest
+        };
+
+        const result = gamblingFunction(testStore);
+
+        expect(result).toEqual(expectedTotal);
+    })
+
+})
+
